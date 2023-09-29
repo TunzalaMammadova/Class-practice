968f5f012f2919488a9a46158aec7c3a125edf92
TunzalaMammadova <tunzalatma@code.edu.az>
2023-09-30 00:25:41 +0400

    added new file master branch

---
+++ ClassPractice/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Employee
+{
+    public string name;
+    public string surname;
+    public string address;
+    public string email;
+    public int age;
+}
+
+
---
+++ ClassPractice/Program.cs
@@ -0,0 +1,69 @@
+﻿
+
+
+
+//CustomMath customMath = new();
+
+//int[] nums = { 2, 4, 5, 3, 7, 8, 6, };
+
+
+//customMath.SumOffOdd(nums);
+
+
+//int a = 65;
+
+//customMath.number(a);
+
+
+//int[] nums = { 1, 4, 5, 3, 7, 2, };
+
+//customMath.SquarNumber(nums);
+
+
+
+//customMath.NumberResult(5);
+
+
+
+Employee employee1 = new()
+{
+    name = "Semed",
+    surname = "Huseynov",
+    address = "Xezer",
+    email = "semed@gmail.com",
+    age = 26
+};
+
+
+
+Employee employee2 = new()
+{
+    name = "Tunzale",
+    surname = "Memmedova",
+    address = "Azadliq",
+    email = "tunzale@gmail.com",
+    age = 24
+};
+
+
+
+Employee employee3 = new()
+{
+    name = "Meryem",
+    surname = "Eliyeva",
+    address = "Yasamal",
+    email = "meryem@gmail.com",
+    age = 23
+};
+
+
+Employee[] employees = { employee1, employee2, employee3 };
+{
+    foreach (var item in employees)
+
+    {
+
+
+    }
+
+}
